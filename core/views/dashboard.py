fbcc5a9008c999aa92d56ea4bce3c51a6e3830a0
sahilkamate03 <45514385+sahilkamate03@users.noreply.github.com>
2024-02-29 17:56:17 +0530

    Add 'created_on' and 'id' fields to file dictionary

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -114,8 +114,10 @@ async def list_files():
             'cTag': file_dict['cTag'][file_dict['cTag'].index('{')+1:file_dict['cTag'].index('}')], 
             'name': file_dict['name'],
             'created_by': file['createdBy']['user']['displayName'],
+            'created_on' : file['fileSystemInfo']['createdDateTime'],
             'last_modified_on': file['fileSystemInfo']['lastModifiedDateTime'],
-            'last_modified_by': file['lastModifiedBy']['user']['displayName']
+            'last_modified_by': file['lastModifiedBy']['user']['displayName'],
+            'id' : file['id']
         })
 
     return jsonify(excel_file_dict)
