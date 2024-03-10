290c06fda8c7a12c43954cdb92ec1c4acbd88f59
sahilkamate03 <45514385+sahilkamate03@users.noreply.github.com>
2024-03-10 23:30:53 +0530

    Update environment variables and database configuration

--- core/models.py
+++ core/models.py
@@ -10,7 +10,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
     def __repr__(self):
         return f"User('{self.id}', '{self.email}')"
